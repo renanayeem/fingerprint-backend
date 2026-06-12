390f8549175526acb9a3e8130a7fe896cb6efa87
renanayeem <renanayeem16@gmail.com>
2026-06-12 15:49:32 +0530

    fix: add SameSite=Strict to JWT cookie to prevent CSRF attacks

--- src/main/java/com/example/fingerprint_backend/JwtUtil.java
+++ src/main/java/com/example/fingerprint_backend/JwtUtil.java
@@ -1,7 +1,7 @@
 package com.example.fingerprint_backend;
 
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
